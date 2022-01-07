8f51fccb306c88cc7610684ce344c0717a443527
Surya Krishna Moorthy <37917817+SuryaKrishnaMoorthy@users.noreply.github.com>
2022-01-07 14:23:29 -0500

    addtour page, accordion, collage

--- src/Pages/Tour.js
+++ src/Pages/Tour.js
@@ -1,5 +1,7 @@
 import { Container, Typography, Box } from "@mui/material";
 import React from "react";
+import CustomizedAccordions from "../components/Accordion";
+import ImageCollage from "../components/ImageCollage";
 
 const Tour = () => {
   return (
@@ -14,7 +16,23 @@ const Tour = () => {
           width={600}
           height={325}
         />
+        <ImageCollage />
       </Box>
+      <Box>
+        <Typography variant="h6" component="h4" marginTop={3}>
+          About this ticket
+        </Typography>
+        <Typography variant="paragraph" component="p" marginTop={3}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+          blanditiis voluptate sit iusto. Commodi excepturi voluptate ea est
+          corrupti obcaecati animi itaque, eum perspiciatis veniam reiciendis
+          ipsa dolore maiores cumque!
+        </Typography>
+      </Box>
+			<Box>
+				<Typography variant="h6" component="h4" marginY={3}>Frequently Asked Questions</Typography>
+				<CustomizedAccordions />
+			</Box>
     </Container>
   );
 };
