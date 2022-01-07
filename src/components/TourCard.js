97184ce7d1b609df35eb04b4fb9bf76f4dd5b0ba
Surya Krishna Moorthy <37917817+SuryaKrishnaMoorthy@users.noreply.github.com>
2022-01-06 21:27:53 -0500

    update tour card

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,22 +1,77 @@
 import React from "react";
-import { Paper, Grid, Typography, Box } from "@mui/material";
+import {
+  Paper,
+  Grid,
+  Typography,
+  Box,
+  Rating,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
 import { AccessTime } from "@mui/icons-material";
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2",
+          },
+          style: {
+            fontSize: 11,
+          },
+        },
+        {
+          props: {
+            variant: "body3",
+          },
+          style: {
+            fontSize: 9,
+          },
+        },
+      ],
+    },
+  },
+});
 
 const TourCard = () => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>
-        <img src="https://picsum.photos/200/300" alt="" className="img" />
-        <Box paddingX={1}>
-          <Typography variant="subtitle1">Immerse into the Falls</Typography>
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            <AccessTime sx={{ width: 12.5 }} />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              5 hours
-            </Typography>
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img src="https://picsum.photos/200/300" alt="" className="img" />
+          <Box paddingX={1}>
+            <Typography variant="subtitle1">Immerse into the Falls</Typography>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <AccessTime sx={{ width: 12.5 }} />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                5 hours
+              </Typography>
+            </Box>
+            <Box sx={{ display: "flex", alignItems: "center" }} marginTop={3}>
+              <Rating
+                name="read-only"
+                value={4.5}
+                precision={0.5}
+                readOnly
+                size="small"
+              />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                4.5
+              </Typography>
+              <Typography variant="body3" component="p" marginLeft={1.5}>
+                (655 reviews)
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6" component="h3">
+                From C $147
+              </Typography>
+            </Box>
           </Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
